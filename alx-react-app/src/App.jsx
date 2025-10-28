88bc5587b23e91dc50f6be4d403b09164d4bc8dd
monylangmading-ops <monylangmading@gmail.com>
2025-10-28 14:37:17 -0700

    Update changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,8 +8,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
       <div>
+         <WelcomeMessage /> 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
