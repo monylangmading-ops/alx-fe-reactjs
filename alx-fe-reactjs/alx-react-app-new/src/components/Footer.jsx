0df52faa141bf9263c53d3b1b2eba06eac11f761
Monylang Mading <monylangmading@gmail.com>
2025-11-29 13:07:42 -0800

    Delete alx-fe-reactjs directory

--- alx-fe-reactjs/alx-react-app-new/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    return(
-        <footer style={{ color: 'yellow', textAlign: 'right', fontSize: '50px'}}>
-  <p>© 2023 City Lovers</p>
-</footer>
-    );
-}
-export default Footer;
