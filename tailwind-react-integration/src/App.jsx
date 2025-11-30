7f56b2137b76753061740832fa2e1a96dd7b6571
monylangmading-ops <monylangmading@gmail.com>
2025-11-29 23:03:49 -0800

    Update userprofile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/userProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <div>
+
+        <UserProfile />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
