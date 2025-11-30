5cf1fd4b9df6df4329394a3cf69fdbaa83b081de
monylangmading-ops <monylangmading@gmail.com>
2025-11-29 23:38:19 -0800

    Changes to UserProfile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,4 +1,4 @@
-import UserProfile from '.src/components/UserProfile'
+import UserProfile from './components/userProfile'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
